efebb9ec3d6641b069516af8fbdb01bc767aa20a
Saint One <whosestone@gmail.com>
2024-08-21 13:32:10 -0700

    submitting commit changes for PDF and main

---
+++ main.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+
+import warnings
+warnings.simplefilter('ignore')
